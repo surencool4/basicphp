0e6b454149533e9aede4c9eda2468ea90b4d8574
surendra <surencool4@gmail.com>
2020-12-28 10:06:42 +0545

    Constants

--- basicphp/3/constants.php
+++ basicphp/3/constants.php
@@ -10,4 +10,29 @@
 		case-insensitive: Specifies whether the constant name should be case-insensitive. Default is false
  */
 
-//Example
+//Example (Case sensative)
+define('nepal', 'South Asian Country');
+echo nepal;
+echo "<hr>";
+
+//Case-insensitive
+define('usa', 'United States of America',true);
+echo UsA;
+echo "<hr>";
+
+//Arrays
+define('cities', [
+				'Kathmandu',
+				'Pokhara',
+				'Janakpur'
+	], true);
+
+echo Cities[1];
+echo "<hr>";
+
+//Constants are automatically global 
+function grab_nepal_constants(){
+	echo nepal;
+}
+//Function Output 
+grab_nepal_constants();
