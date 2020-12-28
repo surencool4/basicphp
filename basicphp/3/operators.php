075ba7331d618bf0c3b2c29d1ca676c75c36f90f
surendra <surencool4@gmail.com>
2020-12-28 12:18:15 +0545

    operators

---
+++ basicphp/3/operators.php
@@ -0,0 +1,30 @@
+<?php 
+
+	//Groups of operators
+
+	//Arithmetic operators
+
+	//Addition
+	echo $addition = 20 + 5;
+	echo "<hr>";
+
+	//Subtraction
+	echo $subtraction = 20 - 5;
+	echo "<hr>";
+
+	//Multipication
+	echo $multiplication = 20 * 5;
+	echo "<hr>";
+
+	//Division
+	echo $division = 20 / 5;
+	echo "<hr>";
+
+	//Moduls
+	echo $modulas = 25 % 50;
+	echo "<hr>";
+	
+	//Result of raising n'th power
+	echo $power = 5 ** 3;
+	echo "<hr>";
+
